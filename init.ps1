ca596df43892fa8ecf7f3fba0cc390b0c94d65b7
przemo <curzytekp@gmail.com>
2017-09-17 13:09:47 -0400

    removed .idea directory

--- init.ps1
+++
@@ -1,7 +0,0 @@
-Set-Location -Path .. -PassThru
-
-$cloneParams = "clone -v https://github.com/Stropek/KattisPythonTemplate.git " +  "Python/" + $args[0]
-Start-Process git -ArgumentList $cloneParams -wait -NoNewWindow -PassThru
-
-$expression = ".\Python\projectSetup.ps1 " + $args[0]
-Invoke-Expression $expression
