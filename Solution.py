07064bd8c5c7771f4ae67c82252fe5d8873bebb8
przemo <curzytekp@gmail.com>
2017-11-12 00:31:36 -0500

    add gitignore; add sample files so that tests pass

--- Solution.py
+++ Solution.py
@@ -4,7 +4,7 @@ import HelperMethods as hm
 class Solution:
 
     def solve(self, in_file_stream, out=sys.stdout):
-        sample_read = hm.readVal(int, in_file_stream)
+        sample_read = hm.readVal(str, in_file_stream)
         hm.write_line(out, "Solve me!")
 
 #########################################
